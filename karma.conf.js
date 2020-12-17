f8719ceb857f9b6dda61ec407efb548434752bb5
dusarah <71254323+dusarah@users.noreply.github.com>
2020-12-17 16:48:06 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -12,6 +12,7 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+    
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
@@ -20,6 +21,7 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
+    
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
